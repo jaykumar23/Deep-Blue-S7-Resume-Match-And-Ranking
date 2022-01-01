ce2e28cb4269dc5ffc0b3c5bca5860d81e6e590c
Karan2310 <karandeep9619@gmail.com>
2022-01-01 15:37:53 +0530

    Routers Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Page2 from './Pages/Page2/Page2';
+import Error from './Pages/Error/Error';
 
 function App() {
   return (
     <div className="App">
-      <h1>Resume Matching and Ranking</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" >
+            <Route index element={<Home />} />
+            <Route path="page2" element={<Page2 />} />
+            <Route path="*" element={<Error />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
